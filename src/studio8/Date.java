d78a7da465fa3d06b8a292b3dfb7940014f7e134
logan <logan@webb-connect-2.dhcp.wustl.edu>
2022-11-17 12:39:04 -0600

    11/17

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,24 @@
 package studio8;
 
 public class Date {
+	private int month; //the months 1-12
+	private int day; //the days 1-31
+	private int year; //year
+	private boolean holidayOrNot; //holiday or not
 
-
+	public Date(int month, int day, int year, boolean holidayOrNot) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.holidayOrNot = holidayOrNot;
+	}
+	
+	public static String toString(int month, int day, int year, boolean holidayOrNot) {
+		return this.month + " / " + this.day + " / " + this.year;
+	}
+	
     public static void main(String[] args) {
-
+    	toString(2, 22, 2222, true);
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,9 +2,22 @@ package studio8;
 
 
 public class Time {
+	private int hour; //1-24
+	private int minute; //1-60
+	private boolean twelveOrTwentyFour; //format of display
 
+	public Time(int hour, int minute, boolean twelveOrTwentyFour) {
+		this.hour = hour;
+		this.minute = minute;
+		this.twelveOrTwentyFour = twelveOrTwentyFour;
+	}
+	
+	public String toString(int hour, int minute, boolean twelveOrTwentyFour) {
+		return this.hour + " / " + this.minute;
+	}
+	
 	public static void main(String[] args) {
- 
+		
     	
     }
 

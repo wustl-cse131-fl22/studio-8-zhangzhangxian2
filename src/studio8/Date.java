9c77f136266502e82d0e98889bd4ffb7ccb3a48b
logan <logan@webb-connect-2.dhcp.wustl.edu>
2022-11-17 12:39:20 -0600

    11/17

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,9 +1,28 @@
 package studio8;
 
+import java.util.Objects;
+
 public class Date {
 	private int month; //the months 1-12
 	private int day; //the days 1-31
 	private int year; //year
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
+	}
+
 	private boolean holidayOrNot; //holiday or not
 
 	public Date(int month, int day, int year, boolean holidayOrNot) {
@@ -13,12 +32,12 @@ public class Date {
 		this.holidayOrNot = holidayOrNot;
 	}
 	
-	public static String toString(int month, int day, int year, boolean holidayOrNot) {
+	public String toString(int month, int day, int year, boolean holidayOrNot) {
 		return this.month + " / " + this.day + " / " + this.year;
 	}
 	
     public static void main(String[] args) {
-    	toString(2, 22, 2222, true);
+    	
     }
 
 }
